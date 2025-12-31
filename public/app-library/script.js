0179e18cd98eedda7fb6bb696d55dddda9115d69
SnezLinkZ Desktop <xd69wtf@gmail.com>
2025-12-30 23:15:04 -0800

    app library

--- public/app-library/script.js
+++ public/app-library/script.js
@@ -1,7 +1,28 @@
 function appCard(app) {
-  const card = $("<div></div>").addClass("app-card").data("href", (app.fromRoot ? "" : "/apps/") + app.url);
+  const card = $("<div></div>").addClass("app-card").data("href", ((app.fromRoot ? "" : "/apps/") + app.url).replace(/\/\//, "/"));
   const favorite = $("<div></div").addClass("app-favorite");
   if (app.favorited) {
     favorite.addClass("favorited");
   }
+  const icon = $("<div></div>").addClass("app-icon");
+  const iconImg = $("<img />").prop("src", "/assets/images/app-icons/" + app.image);
+  const button = $("<div></div>").text(app.name).addClass("app-button").click(function(e) {
+    window.open($(this).parent().data("href"), "_blank");
+  })
+  card.append(favorite, icon.append(iconImg), icon, button);
+  return card;
 }
+
+function renderApps() {
+  const container = $("#apps-container");
+  container.empty();
+  for (const app of window.apps) {
+    const card = appCard(app);
+    container.append(card);
+  }
+}
+
+$(document).ready(async function(e) {
+  window.apps = await fetch("/config/app-library.json").then(r => r.json());
+  renderApps();
+})
